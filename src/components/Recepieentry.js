02ca0c1de2b9c266bfe15f813fb4ac5907fb8957
lekshmi12345 <lekshmisnair213@gmail>
2022-05-29 21:21:41 +0530

    first commit1

--- src/components/Recepieentry.js
+++ src/components/Recepieentry.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const Recepieentry = () => {
+  var [Recepie,setRecepie]=useState("")
+    var [category,setCategory]=useState("")
+    var [Description,setDescription]=useState("")
+    var [preparedby,setPrepared]=useState("")
+    const setData=()=>{
+        var data={"Recepie":Recepie,"category":category,"Description":Description,"preparedby":preparedby}
+        console.log(data)
+    }
   return (
     <div>
         <div className="container">
@@ -9,7 +17,7 @@ const Recepieentry = () => {
             <div className="row g-3">
                 <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" >
                   <label for="" className="form-label">Recepie Title</label>
-                  <input type="text" className="form-control"/>  
+                  <input onChange={(e)=>{setRecepie(e.target.value)}} type="text" className="form-control"/>  
                 </div>
                 <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 ">
                    <label for="" className="form-label">Category </label> 
@@ -20,11 +28,11 @@ const Recepieentry = () => {
                 </div>
                 <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 ">
                     <label for="" className="form-label">Description</label>
-                    <textarea className="form-control" id="" cols="10" rows="4"></textarea>
+                    <textarea  onChange={(e)=>{setDescription(e.target.value)}}className="form-control" id="" cols="10" rows="4"></textarea>
                 </div>
                 <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                    <label for="" className="form-label">Prepared By</label>
-                   <input type="text" className="form-control"/>
+                   <input  onChange={(e)=>{setPrepared(e.target.value)}}type="text" className="form-control"/>
                 </div>
                 <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <button className="btn btn-success">SUBMIT</button>
