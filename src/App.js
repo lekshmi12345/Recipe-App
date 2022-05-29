7ec0c44e51e91c7c2ab145268e91db7f54a854f3
lekshmi12345 <lekshmisnair213@gmail>
2022-05-29 21:46:14 +0530

    first commit1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Recepieentry from './components/Recepieentry';
 import Search from './components/Search';
+import View from './components/View';
 
 function App() {
   return (
     <>
-    
+    <View/>
     <Search/>
     </>
   );
